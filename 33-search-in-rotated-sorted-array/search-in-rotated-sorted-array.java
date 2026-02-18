6a50dd3f4bb01365e9db5607c1e8b1aef002aef7
suhanipandey225-cyber <suhanipandey225@gmail.com>
2026-02-18 22:46:29 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (9.66%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int start = 0;
+        int end = n - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // left sorted
+            if (nums[start] <= nums[mid]) {
+                if (nums[start] <= target && target <= nums[mid]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            } 
+            // right sorted
+            else {
+                if (nums[mid] <= target && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            }
+        }
+
+        return -1; // if not found
+    }
+}
