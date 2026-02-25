65dc9339ea34a830980dbf4f9c4b60a2e9454294
suhanipandey225-cyber <suhanipandey225@gmail.com>
2026-02-25 20:10:41 +0530

    Time: 3 ms (97.92%) | Memory: 52.6 MB (6.14%) - LeetSync

---
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+        
+        int left=0;
+        int maxLength=0;
+        int zerocount=0;
+        for(int right=0; right<nums.length;right++){
+            if(nums[right]==0){
+                zerocount++;
+
+            }
+            while(zerocount>k){
+                if(nums[left]==0){
+                    zerocount--;
+                }
+                left++;
+            }      
+            
+            maxLength= Math.max(maxLength, right-left+1);
+
+        }
+        return maxLength;
+    }
+}
