c8c517a9a729b4dd3b17598ee848b7f2de1d7b7d
suhanipandey225-cyber <suhanipandey225@gmail.com>
2026-03-10 23:08:35 +0530

    Time: 1 ms (99.71%) | Memory: 46.8 MB (53.29%) - LeetSync

---
+++ 3194-find-words-containing-character/find-words-containing-character.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public List<Integer> findWordsContaining(String[] words, char x) {
+        
+        List<Integer> list = new ArrayList<>();
+
+        for(int i = 0; i < words.length; i++){
+            if(words[i].indexOf(x) != -1){
+                list.add(i);
+            }
+        }
+
+        return list;
+    }
+}
