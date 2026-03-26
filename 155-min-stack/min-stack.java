2bbb8a5cfff702f25b378a9d73a918bf96905b85
suhanipandey225-cyber <suhanipandey225@gmail.com>
2026-03-26 10:18:06 +0530

    Time: 5 ms (84.68%) | Memory: 46.9 MB (89.41%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,33 +1,29 @@
 class MinStack {
-    Stack<Integer> stack;
-    Stack<Integer> minStack;
+    Stack<int[]> stack;
 
     public MinStack() {
         stack = new Stack<>();
-        minStack = new Stack<>();
     }
     
     public void push(int val) {
-        stack.push(val);
-        
-        if(minStack.isEmpty() || val <= minStack.peek()) {
-            minStack.push(val);
+        if(stack.isEmpty()) {
+            stack.push(new int[]{val, val});
+        } else {
+            int min = Math.min(val, stack.peek()[1]);
+            stack.push(new int[]{val, min});
         }
     }
     
     public void pop() {
-        if(stack.peek().equals(minStack.peek())) {
-            minStack.pop();
-        }
         stack.pop();
     }
     
     public int top() {
-        return stack.peek();
+        return stack.peek()[0];
     }
     
     public int getMin() {
-        return minStack.peek();
+        return stack.peek()[1];
     }
 }
 
