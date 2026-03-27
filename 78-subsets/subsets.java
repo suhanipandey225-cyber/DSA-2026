ef6ca95a792e56ef26d5bb7a1ec875fe1bec5d8d
suhanipandey225-cyber <suhanipandey225@gmail.com>
2026-03-27 10:16:14 +0530

    Time: 1 ms (88.27%) | Memory: 43.9 MB (92.41%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,26 @@
+class Solution {
+    public static void Solve(int i , int[] nums ,  List<List<Integer>> ans , List<Integer> curr)
+    {
+        if(i == nums.length)
+        {
+            ans.add(new ArrayList<>(curr)) ;
+            return ;
+        }
+
+        // take
+        curr.add(nums[i]) ;
+        Solve(i+1,nums,ans,curr) ;
+
+        curr.remove(curr.size()-1) ;
+        Solve(i+1,nums,ans,curr) ;
+        // return ans ;
+
+
+    }
+    public List<List<Integer>> subsets(int[] nums) {
+        
+        List<List<Integer>> ans = new ArrayList() ;
+        Solve(0,nums,ans,new ArrayList<Integer>()) ;
+        return ans ;
+    }
+}
