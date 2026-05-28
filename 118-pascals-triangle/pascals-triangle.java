3938527e7d09c8a1fbe78fab481d3d87aa4fb11c
suhanipandey225-cyber <suhanipandey225@gmail.com>
2026-05-28 21:51:05 +0530

    Time: 1 ms (95.17%) | Memory: 43.6 MB (15.45%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            
+            List<Integer> row = new ArrayList<>();
+
+            for (int j = 0; j <= i; j++) {
+                
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    int value = ans.get(i - 1).get(j - 1) 
+                              + ans.get(i - 1).get(j);
+                    row.add(value);
+                }
+            }
+
+            ans.add(row);
+        }
+
+        return ans;
+    }
+}
