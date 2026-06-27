ff215f04557039a5072279c4827e5ca1776c0ded
suhanipandey225-cyber <suhanipandey225@gmail.com>
2026-06-27 19:32:07 +0530

    Time: 3 ms (14.34%) | Memory: 50.6 MB (5.69%) - LeetSync

---
+++ 143-reorder-list/reorder-list.java
@@ -0,0 +1,33 @@
+class Solution {
+    public void reorderList(ListNode head) {
+
+        if (head == null || head.next == null)
+            return;
+
+        ArrayList<ListNode> list = new ArrayList<>();
+
+        ListNode temp = head;
+
+        while (temp != null) {
+            list.add(temp);
+            temp = temp.next;
+        }
+
+        int left = 0;
+        int right = list.size() - 1;
+
+        while (left < right) {
+
+            list.get(left).next = list.get(right);
+            left++;
+
+            if (left == right)
+                break;
+
+            list.get(right).next = list.get(left);
+            right--;
+        }
+
+        list.get(left).next = null;
+    }
+}
