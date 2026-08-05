ad21d7b61e3425a552dfa1275e9db2636d322a86
suhanipandey225-cyber <suhanipandey225@gmail.com>
2026-08-05 05:58:41 +0530

    Time: 0 ms (100.00%) | Memory: 47.3 MB (49.25%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public int missingNumber(int[] nums) {
+
+        int ans = nums.length;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            ans ^= i;
+            ans ^= nums[i];
+
+        }
+
+        return ans;
+    }
+}
