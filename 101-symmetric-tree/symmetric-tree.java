4b6aaf4c5d818a8288c37102e672302ae6ca8982
suhanipandey225-cyber <suhanipandey225@gmail.com>
2026-08-26 06:02:41 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (33.48%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return mirror(root.left, root.right);
+    }
+
+    private boolean mirror(TreeNode left, TreeNode right) {
+
+        if (left == null && right == null) {
+            return true;
+        }
+
+        if (left == null || right == null) {
+            return false;
+        }
+
+        if (left.val != right.val) {
+            return false;
+        }
+
+        return mirror(left.left, right.right)
+            && mirror(left.right, right.left);
+    }
+}
